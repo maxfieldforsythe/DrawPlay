d0b53c80bcf3e6ee0a620455575e6494a1bf7821
Maxfield Forsythe <maxfieldforsythe@mines.edu>
2019-09-26 10:16:29 -0600

    Changed head size to 50

--- src/Cat.java
+++ src/Cat.java
@@ -5,8 +5,8 @@ import java.awt.geom.*;
 public class Cat {
 	// drawing constants are private - noone needs to know what we're doing
 	// pick a head dimension
-	private static final int HEAD_DIMENSION = 100;
-	// eyes will be about 1/4 from top of head and 1/4 from left
+	private static final int HEAD_DIMENSION = 50;
+	// eyes will be about 1/4 from top of head and 1/3 from left
 	private static final int EYE_Y = HEAD_DIMENSION/3;
 	private static final int EYE_X = HEAD_DIMENSION/3;
 	private static final int EYE_SEPARATION = HEAD_DIMENSION/3;
